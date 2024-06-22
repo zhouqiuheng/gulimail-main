c28918d5c11b465003f497e609aaf5688e5898ce
zqh <2283567366@qq.com>
2024-06-22 18:39:45 +0800

    项目初始结构创建

--- gulimail-product/src/main/java/com/zqh/gulimail/product/service/impl/AttrGroupServiceImpl.java
+++ gulimail-product/src/main/java/com/zqh/gulimail/product/service/impl/AttrGroupServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zqh.gulimail.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -24,6 +25,34 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        if(catelogId == 0){
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<AttrGroupEntity>()
+            );
+            return new PageUtils(page);
+        }else{
+            String key = (String) params.get("key");
+            //有模糊查询，及匹配字段中的其中一段
+            //select * from pms_attr_grop where catelog_id = ? and ( attr_group_id=key or attr_group_name like %key%)
+
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<AttrGroupEntity>().eq("catelog_id",catelogId);
+            if(!StringUtils.isEmpty(key)){
+                wrapper.and((obj)->{
+                    obj.eq("attr_group_id",key).or().like("attr_group_name",key);
+                });
+            }
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    wrapper
+            );
+            return new PageUtils(page);
+
+        }
     }
 
 }
