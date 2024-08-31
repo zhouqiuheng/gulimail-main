39c666eed1f8dbba13572d8110c0e6daf5dc005e
zqh <2283567366@qq.com>
2024-09-01 01:51:16 +0800

    项目初始结构创建

--- gulimail-product/src/main/java/com/zqh/gulimail/product/service/SpuInfoService.java
+++ gulimail-product/src/main/java/com/zqh/gulimail/product/service/SpuInfoService.java
@@ -2,6 +2,7 @@ package com.zqh.gulimail.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.zqh.common.utils.PageUtils;
+import com.zqh.gulimail.product.entity.SpuInfoDescEntity;
 import com.zqh.gulimail.product.entity.SpuInfoEntity;
 import com.zqh.gulimail.product.vo.SpuSaveVo;
 
@@ -19,5 +20,8 @@ public interface SpuInfoService extends IService<SpuInfoEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     void saveSpuInfo(SpuSaveVo vo);
+
+    void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity);
+
 }
 
--- gulimail-product/src/main/java/com/zqh/gulimail/product/service/impl/SpuInfoServiceImpl.java
+++ gulimail-product/src/main/java/com/zqh/gulimail/product/service/impl/SpuInfoServiceImpl.java
@@ -1,8 +1,18 @@
 package com.zqh.gulimail.product.service.impl;
 
+import com.zqh.gulimail.product.entity.*;
+import com.zqh.gulimail.product.service.*;
+import com.zqh.gulimail.product.vo.BaseAttrs;
 import com.zqh.gulimail.product.vo.SpuSaveVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -10,13 +20,23 @@ import com.zqh.common.utils.PageUtils;
 import com.zqh.common.utils.Query;
 
 import com.zqh.gulimail.product.dao.SpuInfoDao;
-import com.zqh.gulimail.product.entity.SpuInfoEntity;
-import com.zqh.gulimail.product.service.SpuInfoService;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("spuInfoService")
 public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> implements SpuInfoService {
+
+    @Autowired
+    SpuInfoDescService spuInfoDescService;
+
+    @Autowired
+    SpuImagesService spuImagesService;
+
+    @Autowired
+    AttrService attrService;
+
+    @Autowired
+    ProductAttrValueService productAttrValueService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -32,16 +52,55 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     @Override
     public void saveSpuInfo(SpuSaveVo vo) {
         //保存spu基本信息pms_spu_info
+        SpuInfoEntity spuInfoEntity = new SpuInfoEntity();
+        BeanUtils.copyProperties(vo,spuInfoEntity);
+        spuInfoEntity.setCreateTime(new Date());
+        spuInfoEntity.setUpdateTime(new Date());
+        this.saveBaseSpuInfo(spuInfoEntity);
 
-        //保存spu的描述图片pms_spu_info_desc
+
+        //保存spu的描述图片pms_spu_info_desc//描述图片是一段一段的，可以用“，"分割
+        List<String> decript = vo.getDecript();
+        SpuInfoDescEntity spuInfoDescEntity = new SpuInfoDescEntity();
+        spuInfoDescEntity.setSpuId(spuInfoEntity.getId());
+
+        spuInfoDescEntity.setDecript(String.join(",",decript));
+        spuInfoDescService.saveSpuInfoDesc(spuInfoDescEntity);
+
 
         //保存spu的图片集pms_spu_images
-
+        List<String> images = vo.getImages();
+        spuImagesService.saveImages(spuInfoEntity.getId(),images);
         //保存spu的规格参数pms_product_attr_value
+        List<BaseAttrs> baseAttrs = vo.getBaseAttrs();
+        List<ProductAttrValueEntity> collect = baseAttrs.stream().map(attr -> {
+            ProductAttrValueEntity valueEntity = new ProductAttrValueEntity();
+            valueEntity.setAttrId(attr.getAttrId());
+            AttrEntity attrEntity = attrService.getById(attr.getAttrId());
+            String attrName = attrEntity.getAttrName();
+            valueEntity.setAttrName(attrName);
+            valueEntity.setAttrValue(attr.getAttrValues());
+            valueEntity.setQuickShow(attr.getShowDesc());
+            valueEntity.setSpuId(spuInfoEntity.getId());
+
+            return valueEntity;
+        }).collect(Collectors.toList());
+
+        productAttrValueService.saveProductAttr(collect);
+
+        //保存spu积分信息gulimail_sms.sms_spu_bounds
 
         //保存当前spu对应的所有sku信息
         //1）sku基本信息pms_sku_info
         //2）sku图片信息pms_sku_images
+        //3）sku销售属性信息pms_sku_sale_attr_value
+        //4)sku优惠满减信息gulimail_sms.sms_sku_full_reduction/sms_sku_ladder/sms_member_pric
     }
+
+    @Override
+    public void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity) {
+        this.baseMapper.insert(spuInfoEntity);
+    }
+
 
 }
